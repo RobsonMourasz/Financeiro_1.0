20056abaf80ea89a60f157f67e797af70f2a22f0
RobsonMourasz <robsonic10@gmail.com>
2025-02-17 18:25:32 -0300

    Fazendo o INDEX do login

--- app/assets/php/Request/Login.php
+++ app/assets/php/Request/Login.php
@@ -1,6 +1,30 @@
 <?php
+    include_once __DIR__.'/../../../data/database.php';
+
     if (isset($_POST)) {
         $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
         $senha = $_POST['senha'];
+        $tempPassword = password_hash($senha, PASSWORD_DEFAULT);
+
+        try {
+            $BuscaUsuario = $conexao->query(" SELECT * FROM caduser WHERE EmailUser like '%$email%' ");
+
+            if ($BuscaUsuario->num_rows > 0) {
+                $User = $BuscaUsuario->fetch_all(MYSQLI_ASSOC); 
+                $verifyPassword = $conexao->query("SELECT * FROM caduser WHERE EmailUser like '%$email%'");
+                $verifyPassword = $verifyPassword->fetch_all(MYSQLI_ASSOC);
+                if(password_verify($tempPassword, $verifyPassword['SenhaUser'])){
+                    ?> <script> location.assign("app/index.php") </script> <?php
+                }else{
+                    ?> <script> alert("Senha incorreta!") </script> <?php        
+                }
+
+            }else{
+            ?> <script> alert("Usuario não encontrado!") </script> <?php
+            }
+
+        } catch (\Throwable $th) {
+            throw $th;
+        }
 
     }
